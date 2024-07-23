05b83eec74719fe40922394daa6f76f2ffde4714
Ryan Trauntvein <djdefi@gmail.com>
2024-07-23 12:01:01 -0700

    Tests (#3)
    * Tests
    
    * Update test_trailer_weight.yml
    
    * Test fix
    
    * Update test_trailer_weight.yml

--- test_trailer_weight.rb
+++ test_trailer_weight.rb
@@ -1,6 +1,6 @@
 # test_trailer_weight.rb
 
-require './trailer_weight.rb'
+require 'optparse'
 require 'rspec'
 
 RSpec.describe "TrailerWeightCalculator" do
@@ -10,9 +10,27 @@ RSpec.describe "TrailerWeightCalculator" do
       cargo: [200, 150, 50]
     }
 
+    # Mocking command-line options for testing
+    allow_any_instance_of(OptionParser).to receive(:parse!).and_return(options)
+
+    # Capture stdout output during script execution
+    output = capture_output { require './trailer_weight.rb' }
+
+    # Calculate expected gross trailer weight based on mock options
     expected_gross_trailer_weight = ((options[:max_weight] - options[:cargo].sum) / 0.13).round
 
-    # Redirect STDOUT to capture puts output
-    expect { calculate_trailer_weight(options) }.to output(/Max towable gross trailer weight: #{expected_gross_trailer_weight}/).to_stdout
+    # Verify script output contains expected trailer weight message
+    expect(output).to include("Max towable gross trailer weight: #{expected_gross_trailer_weight}")
   end
+end
+
+# Helper method to capture stdout for testing purposes
+def capture_output(&block)
+  original_stdout = $stdout
+  output_catcher = StringIO.new
+  $stdout = output_catcher
+  yield
+  output_catcher.string
+ensure
+  $stdout = original_stdout
 end
